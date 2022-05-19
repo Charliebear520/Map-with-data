2e090eaa7647a5baabe5e149d7faa1062b952602
Charliebear <50406s97116@gmail.com>
2022-05-19 11:32:00 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { NativeBaseProvider } from "native-base";
+
+import MapScreen from "./src/screens/MapScreen";
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NativeBaseProvider>
+        <MapScreen />
+      </NativeBaseProvider>
+    </SafeAreaProvider>
+  );
+}
